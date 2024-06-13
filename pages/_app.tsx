8e188a049a8c952db10a91165a4c854b4b3c9d0a
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-06-13 16:43:19 -0700

    package update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { PrefetchProvider } from '../contexts/PrefetchStats';
 import '../styles/globals.css';  
+import { Analytics } from "@vercel/analytics/react"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
