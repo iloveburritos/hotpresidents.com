b6e376d70727e47bd8c70a83206e80805f223dc8
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-08-30 21:15:19 -0700

    shuffle fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost'],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ['localhost', 'www.hotpresidents.com'],
+  },
+};
+
+export default nextConfig;
