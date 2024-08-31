0d0d65f4692b13c66e7f44f7bb756e30aa4d8f36
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-08-30 21:31:00 -0700

    vercel insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { PrefetchProvider } from '../contexts/PrefetchStats';
 import '../styles/globals.css';  
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <PrefetchProvider>            
         <Component {...pageProps} />
+        <SpeedInsights />
+        <Analytics />
         </PrefetchProvider>
     );
 };
