2e4cdc12aa210ddabe32c0a553978dba6ea3c849
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-09-02 16:01:07 -0700

    reverted to webp

--- components/PresidentCard.tsx
+++ components/PresidentCard.tsx
@@ -1,40 +1,24 @@
 // components/PresidentCard.tsx
-import React, { useState } from 'react'
-import { President } from '../models/presidents'
-import PreloadImage from './PreloadImage'
+import React from 'react';
+import { President } from '../models/presidents';
 
 interface PresidentCardProps {
-  president: President
-  nextPresidentImage?: string
+    president: President;
 }
 
-const PresidentCard: React.FC<PresidentCardProps> = ({ president, nextPresidentImage }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
-  const images = [president.imageURL, ...(president.alternativeImages || [])]
+const PresidentCard: React.FC<PresidentCardProps> = ({ president }) => {
+    return (
+        <div id="container" >
+            <div id="img-container">
+                <img src={president.imageURL} alt={`Photo of ${president.name}`} />
+            </div>
+            <div id="subtext-container">
+                <h2>{president.name}</h2>
+                <p>{president.office}: {president.yearsInOffice}</p>
+                <p>{president.quote || "Oh Notus - no quote available"}</p>
+            </div>
+        </div>
+    );
+};
 
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
-  }
-
-  return (
-    <div id="container">
-      <div id="img-container" onClick={nextImage}>
-        <PreloadImage
-          src={images[currentImageIndex]}
-          preloadSrc={nextPresidentImage}
-          alt={`Photo of ${president.name}`}
-          width={300}
-          height={400}
-          layout="responsive"
-        />
-      </div>
-      <div id="subtext-container">
-        <h2>{president.name}</h2>
-        <p>{president.office}: {president.yearsInOffice}</p>
-        <p>{president.quote || "Oh Notus - no quote available"}</p>
-      </div>
-    </div>
-  )
-}
-
-export default PresidentCard
+export default PresidentCard;
