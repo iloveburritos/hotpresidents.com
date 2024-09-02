e4a784c6aa68f2dc40469715f7ad38949bd8aea9
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-09-02 15:41:52 -0700

    removed image size in presidentcard

--- components/PresidentCard.tsx
+++ components/PresidentCard.tsx
@@ -27,7 +27,6 @@ const PresidentCard: React.FC<PresidentCardProps> = ({ president, nextPresidentI
           alt={`Photo of ${president.name}`}
           fill
           style={{ objectFit: 'contain' }}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
       <div className="text-center">
