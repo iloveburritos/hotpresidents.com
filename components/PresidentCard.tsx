b0aa5a32275b1561915feedf6978bdbb95fc7a2a
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-09-03 15:36:34 -0700

    removed loading from president card

--- components/PresidentCard.tsx
+++ components/PresidentCard.tsx
@@ -5,20 +5,18 @@ import { President } from '../models/presidents';
 
 interface PresidentCardProps {
     president: President;
-    
 }
 
 const PresidentCard: React.FC<PresidentCardProps> = React.memo(({ president }) => {
     return (
         <div id="container">
-            <div id="img-container" >
+            <div id="img-container">
                     <Image 
                         src={president.imageURL} 
                         alt={`Photo of ${president.name}`} 
                         width={500} 
                         height={500} 
                         priority
-                        className="rounded-lg"
                     />
             </div>
             <div id="subtext-container">
